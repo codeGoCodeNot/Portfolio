a0443954d83d00b945f18f1243961df19045b2b7
sendosend <johnsenberdin2930@gmail.com>
2025-09-17 20:28:39 +0800

    adding addBtn which calls renderOrderfunction in order to show the order by customers

--- Restaurant Ordering App/index.js
+++ Restaurant Ordering App/index.js
@@ -1,5 +1,46 @@
 import menuArray from "./data.js";
 
+const order = [];
+
+const addBtn = () => {
+  document.querySelectorAll(".add-btn").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const menuId = Number(e.target.dataset.id);
+      const targetId = menuArray.find((item) => item.id === menuId);
+      if (targetId) {
+        order.push(targetId);
+        renderOrder();
+      }
+    });
+  });
+};
+
+const renderOrder = () => {
+  const total = order.reduce((total, current) => total + current.price, 0);
+  let html = `<h1 class="order-title" >Your Order</h1>`;
+
+  order.forEach((menu) => {
+    html += `
+          <div class="order-item">
+          <div class="item">
+            <h2>${menu.name}</h2>
+            <span><p>remove</p></span>
+            <p class="order-price">$${menu.price}</p>
+          </div>
+          `;
+  });
+  html += `
+        <hr class="secondary-hr" />
+          <div class="item">
+            <h2>Total Price:</h2>
+            <p class="order-price">$${total}</p>
+          </div>
+        </div>
+        `;
+
+  document.querySelector(".order-container").innerHTML = html;
+};
+
 const renderHtml = () => {
   return menuArray
     .map((menu) => {
@@ -11,7 +52,7 @@ const renderHtml = () => {
 
             <ul>
               <li><h2>${name}</h2></li>
-              <li><p>${ingredients}/p></li>
+              <li><p>${ingredients.join(", ")}</p></li>
               <li>$${price}</li>
             </ul>
           </div>
@@ -25,6 +66,7 @@ const renderHtml = () => {
 
 const render = () => {
   document.querySelector(".menu-container").innerHTML = renderHtml();
+  addBtn();
 };
 
 render();
